a5b16a535c70374c4959984b4853c4dee43b5932
Samson Eduotu <ogheneinfinitea@gmail.com>
2018-12-09 13:48:37 -0500

    created new files for courses

---
+++ app/src/main/java/com/example/root/fingerprint/com404.java
@@ -0,0 +1,13 @@
+package com.example.root.fingerprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class com404 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com404);
+    }
+}
--- app/src/main/java/com/example/root/fingerprint/list_course.java
+++ app/src/main/java/com/example/root/fingerprint/list_course.java
@@ -2,6 +2,7 @@ package com.example.root.fingerprint;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -9,24 +10,44 @@ import android.widget.ProgressBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class list_course extends AppCompatActivity {
     ListView listview_course;
     ProgressDialog progressDialog;
     FirebaseAuth auth;
+    DatabaseReference databaseCourse;
+
+    List<Course> course;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_course);
+        databaseCourse = FirebaseDatabase.getInstance().getReference("course");
 
-        auth = FirebaseAuth.getInstance();
+        listview_course =(ListView)findViewById(R.id.listview_course);
 
-        FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
-        if(user!=null)
-        {
-            Intent i = new Intent( list_course.this,MenuDrawer.class);
-        }
 
+    }
+    protected void onStart(){
+        super.onStart();
+
+        databaseCourse.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
